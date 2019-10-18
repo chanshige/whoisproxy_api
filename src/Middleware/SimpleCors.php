366567a0fc2f1861977a2c59125e671cda6b70dc
Shigeki Tanaka <dev@shigeki.tokyo>
2019-10-18 14:39:48 +0900

    update

--- src/Middleware/SimpleCors.php
+++ src/Middleware/SimpleCors.php
@@ -36,7 +36,7 @@ final class SimpleCors implements MiddlewareInterface
 
         return $response
             ->withHeader('Access-Control-Allow-Origin', $this->allowOrigin)
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin')
             ->withHeader('Access-Control-Allow-Methods', 'GET');
     }
 }
